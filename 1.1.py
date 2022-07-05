ea015e112da617b288e6c73ab67db7e3281f2516
Самохина Вероника <samohina@tochka.com>
2022-07-06 01:49:34 +0500

    add first solution

---
+++ 1.1.py
@@ -0,0 +1,11 @@
+'''
+Write a function called solution(data, n) that takes in a list of less 
+than 100 integers and a number n, and returns that same list but with 
+all of the numbers that occur more than n times removed entirely
+'''
+
+def solution(data, n):
+    repeated_numb = list(set([x for x in data if data.count(x) > n]))
+    result = [val for val in data if val not in repeated_numb]
+    
+    return(result)
